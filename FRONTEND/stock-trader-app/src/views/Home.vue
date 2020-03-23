0050dcf18142847695541a14ba0d4818b04ed3a4
TristanMoe <Trilleo@hotmail.com>
2020-03-23 21:56:31 +0100

    Added vue projekt with bootstrap, vue-router and vuex

---
+++ FRONTEND/stock-trader-app/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+   <nav class="navbar navbar-expand-lg navbar-dark bg-success">
+  <a class="navbar-brand" href="#">Stock Trader App</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link" href="#">Create User <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">Inlist Stock</a>
+      <a class="nav-item nav-link" href="#">Stock Market</a>
+    </div>
+  </div>
+</nav>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+};
+</script>
